9d1eedd38f80be9fd33243dd5a3d4cff641ac86a
lavanmetta <lavanmetta111@gmail.com>
2023-07-08 18:47:22 +0530

    py

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-# how to swap two variables
-
-variable_one = "Blue Color"
-variable_two = "Green Color"
-
-# approach one
-temp_var = variable_two  # green color
-variable_two = variable_one  # blue color
-variable_one = temp_var   # green color
-
-print("blue color to : ", variable_one)
-print("green color to : ", variable_two)
-
-# approach 2
-
-variable_one , variable_two = variable_two, variable_one
-
-print(variable_one)
-print(variable_two)
 
 
 # how to check given number is prime number or not ?
@@ -26,3 +7,16 @@ print(variable_two)
     is defined as a prime number. Consider an example of number 5,
     which has only two factors 1 and 5. This means it is a prime number. """
 
+
+number = int(input())
+
+
+if number > 1:
+    for i in range(2, number):
+        if number % i == 0:
+            print(number, "is Not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+else:
+    print(number, "is Not a prime number")
