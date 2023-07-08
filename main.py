20c2221a9aa0a944d8c662804dbc621414d0b83c
lavanmetta <lavanmetta111@gmail.com>
2023-07-08 18:03:17 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# how to check given number is prime number or not ?
+
+
+"""  Any whole number greater than 1 that is divisible only by 1 and itself,
+    is defined as a prime number. Consider an example of number 5,
+    which has only two factors 1 and 5. This means it is a prime number. """
+
